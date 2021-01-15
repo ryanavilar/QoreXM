cc3bd05339efc8762fe7e317be4edc9b7f060c38
Ryan <ryan@Ryans-MacBook-Pro.local>
2021-01-16 04:00:16 +0700

    trigger

--- pages/brand.tsx
+++ pages/brand.tsx
@@ -23,7 +23,7 @@ export default function Home() {
                 content={
                     <>
                         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                            <h1 className="text-2xl font-semibold text-gray-900">Brand</h1>
+                            <h1 className="text-2xl font-semibold text-gray-900">Brand Page</h1>
                         </div>
                         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                             <div className="py-4">
